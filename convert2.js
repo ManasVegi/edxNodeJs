c4ec9dee1f42373325c23238f050ff849dac370a
Manas Vegi <manasvegi@gmail.com>
2020-06-22 16:16:05 +0800

    upload solution

---
+++ convert2.js
@@ -0,0 +1,13 @@
+const csvjson = require('csvjson')
+const fs = require('fs')
+const path = require('path')
+
+fs.readFile(path.join(__dirname, 'customer-data.xls'), {encoding: 'utf-8'}, (error, data) => {
+    if (error) {
+        return console.error('The error is: ', error)
+    }
+    //console.log(data)
+    const jsonObj = csvjson.toObject(data)
+    const result = JSON.stringify(jsonObj)
+    fs.writeFileSync(path.join(__dirname, 'customer-data.json'), result)
+})
